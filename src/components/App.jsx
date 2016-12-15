d36a3f27642f50620d1a265803d8e38770292290
Bjorn John Y. Ramos <bjyramos@gmail.com>
2016-12-14 17:14:37 -0800

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import QuestionList from './quiz/QuestionList.jsx'
+
+class App extends Component{
+	
+	render(){
+		return(
+			<div>
+				APP 
+			</div>
+			)
+	}
+}
+
+export default App
